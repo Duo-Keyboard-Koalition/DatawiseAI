f9879aa624fd96c82a7a70a4d23b10d60588c357
Darcy Liu <darcy.ldx@gmail.com>
2024-10-11 01:02:04 -0400

    changed to Dashboard

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
-import App from './Dashboard';
+import Dashboard from './Dashboard';
 
 ReactDOM.createRoot(document.querySelector("#root")!).render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
-      <App />
+      <Dashboard />
     </StyledEngineProvider>
   </React.StrictMode>
 );
